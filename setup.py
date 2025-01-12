6dbcc9abb0307d854a394d407f63af98b7a1c01f
mexhanic69 <onlinebusiness898@gmail.com>
2025-01-12 17:27:06 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import os
-os.system('sudo apt-get install python2.7')  #sudo...just incase a fella runs it on desktop environmet with permmision involved.
-os.system('sudo apt-get install python-pip')
-os.system('pip2 install --upgrade google-api-python-client')
-os.system('pip2 install --upgrade google-auth-oauthlib google-auth-httplib2')
 
+# Install dependencies for Python 3 in Google Colab
+os.system('apt-get update')  # Update package lists
+
+# Install Python 3
+os.system('apt-get install python3')  # Install Python 3
+
+# Install pip for Python 3
+os.system('apt-get install python3-pip')  # Install pip3 for Python 3
+
+# Upgrade required packages using pip3
+os.system('pip3 install --upgrade google-api-python-client')  # Upgrade the Google API client
+os.system('pip3 install --upgrade google-auth-oauthlib google-auth-httplib2')  # Upgrade OAuth and HTTP library
